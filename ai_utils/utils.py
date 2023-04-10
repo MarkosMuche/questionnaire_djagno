2730f3e64622f518fd4286eed4acc343818ec211
solomon <amsalusolomon12@gmail.com>
2023-04-10 17:21:50 +0300

    vision statememet generation done but we need more prompt engineering.

--- ai_utils/utils.py
+++ ai_utils/utils.py
@@ -1,2 +1,87 @@
 
 
+from langchain import OpenAI, LLMChain, PromptTemplate
+from langchain.memory import ConversationBufferWindowMemory
+from dotenv import load_dotenv
+
+load_dotenv()
+
+
+template_insight = """Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+
+Human: {human_input}
+Assistant:"""
+
+template_vision = """Assistant is a large language model trained by OpenAI.
+
+Assistant is designed to be able to assist with a wide range of tasks, from answering simple questions to providing in-depth explanations and discussions on a wide range of topics. As a language model, Assistant is able to generate human-like text based on the input it receives, allowing it to engage in natural-sounding conversations and provide responses that are coherent and relevant to the topic at hand.
+
+Assistant is constantly learning and improving, and its capabilities are constantly evolving. It is able to process and understand large amounts of text, and can use this knowledge to provide accurate and informative responses to a wide range of questions. Additionally, Assistant is able to generate its own text based on the input it receives, allowing it to engage in discussions and provide explanations and descriptions on a wide range of topics.
+
+Overall, Assistant is a powerful tool that can help with a wide range of tasks and provide valuable insights and information on a wide range of topics. Whether you need help with a specific question or just want to have a conversation about a particular topic, Assistant is here to assist.
+
+
+Human: {human_input}
+Assistant:"""
+
+prompt_insight = PromptTemplate(
+    input_variables=[ "human_input"], 
+    template=template_insight
+)
+prompt_vision = PromptTemplate(
+    input_variables=["human_input"], 
+    template=template_insight
+)
+
+
+llm_insight = OpenAI(temperature=0, model_name='text-davinci-003')
+llm_vision = OpenAI(temperature=0, model_name='text-davinci-003')
+
+chain_insight = LLMChain(
+    llm=llm_insight,
+    prompt=prompt_insight, 
+    verbose=True, 
+)
+
+chain_vision = LLMChain(
+    llm=llm_insight,
+    prompt=prompt_vision,
+    verbose=True, 
+)
+
+def get_vision_statement(data):
+    
+    insights = chain_insight.predict(human_input=data)
+
+    vision_statement = chain_vision.predict(human_input = insights)
+
+    return vision_statement
+
+
+
+
+
+
+
+def get_chatbot_response(prompt):
+
+    response = chain_insight.predict(human_input=prompt)
+    return response
+
+
+
+if __name__ == "__main__":
+
+    path = 'sample.txt'
+    with open(path, "r") as file:
+        data = file.read()
+
+    vision_statement = get_vision_statement(data=data)
+    print(vision_statement)
--- app/views.py
+++ app/views.py
@@ -1,9 +1,16 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from ai_utils.utils import get_vision_statement
+
 
 class MainView(APIView):
 
     def post(self, request):
 
         data = request.data
+
+        get_vision_statement(data)
+
+        return Response(data)
         
