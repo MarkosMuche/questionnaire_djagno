a2d40a2f36767440529aa9af64fa87a2e20829ed
MarkosMuche <markosmuche2018@gmail.com>
2023-06-06 22:08:30 +0300

    spelling error in questionnaire removed.

--- questionnaire_djagno/asgi.py
+++ questionnaire_djagno/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for quetionaire_djagno project.
+ASGI config for questionnaire_djagno project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quetionaire_djagno.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'questionnaire_djagno.settings')
 
 application = get_asgi_application()
--- questionnaire_djagno/wsgi.py
+++ questionnaire_djagno/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for quetionaire_djagno project.
+WSGI config for questionnaire_djagno project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'quetionaire_djagno.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'questionnaire_djagno.settings')
 
 application = get_wsgi_application()
