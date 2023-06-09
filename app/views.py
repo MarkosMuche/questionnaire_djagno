7bb80ae57a8154e87c110b87a78203a50dcdd719
MarkosMuche <markosmuche2018@gmail.com>
2023-06-09 11:59:35 +0300

    Add email sending feature.

--- app/views.py
+++ app/views.py
@@ -1,7 +1,8 @@
-from django.shortcuts import render
+
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from ai_utils.utils import get_vision_statement
+from django.core.mail import send_mail
 
 
 class MainView(APIView):
@@ -14,6 +15,13 @@ class MainView(APIView):
 
         # vision_statement = get_vision_statement(data)
         vision_statement = data
+        send_mail(
+            'Your Vision Statement',
+            vision_statement['data'],
+            'markosmuche2018@gmail.com',
+            ['markos@mindlink.space'],
+            fail_silently=False,
+        )
 
         return Response(vision_statement)
         
--- questionnaire_djagno/settings.py
+++ questionnaire_djagno/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -123,3 +124,14 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# settings.py
+
+# settings.py
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'  # Gmail's SMTP server
+EMAIL_PORT = 587  # Gmail's SMTP port for TLS
+EMAIL_USE_TLS = True  # Use TLS encryption for the connection
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
