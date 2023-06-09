6595bb07363dafea58c4bd61a97b76d4a4245d57
solomon <amsalusolomon12@gmail.com>
2023-06-09 16:00:41 +0300

    emailsending work

--- app/views.py
+++ app/views.py
@@ -3,25 +3,41 @@ from rest_framework.views import APIView
 from rest_framework.response import Response
 from ai_utils.utils import get_vision_statement
 from django.core.mail import send_mail
+from dotenv import load_dotenv
+from email.message import EmailMessage
+import ssl
+import smtplib
+from decouple import Config
+import json
+
 
 
 class MainView(APIView):
 
     def post(self, request):
-
         data = request.data
-        print('////////////////////////////////////////////////////////////////////')
         print(data)
-
+        email=data['Email']
+        print(email)
+        config = Config('.env')
+        print('solomon')
         # vision_statement = get_vision_statement(data)
+        email_sender=config("email_sender")
         vision_statement = data
-        send_mail(
-            'Your Vision Statement',
-            vision_statement['data'],
-            'markosmuche2018@gmail.com',
-            ['markos@mindlink.space'],
-            fail_silently=False,
-        )
+        email_password=config("email_password")
+        email_receiver=email
 
-        return Response(vision_statement)
-        
+        subject='your vision'
+        body = json.dumps(vision_statement)
+
+        em=EmailMessage()
+        em['From']=email_sender
+        em['To']=email_receiver
+        em['subject']=subject
+        em.set_content(body)
+
+        context=ssl.create_default_context()
+        with smtplib.SMTP_SSL('smtp.gmail.com',465,context=context) as smtp:
+            smtp.login(email_sender,email_password)
+            smtp.sendmail(email_sender,email_receiver,em.as_string())
+        return Response('')       
